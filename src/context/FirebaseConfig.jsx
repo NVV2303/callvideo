3b5250850a1a3ea4485bf5cdbe164726cb402604
anhvotaday <vovatva01@gmail.com>
2025-03-28 16:48:33 +0700

    First commit

---
+++ src/context/FirebaseConfig.jsx
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+// Firebase configuration
+const firebaseConfig = { 
+  apiKey : "AIzaSyBxdLR1kbQBSzx8-VD4Rnc7xxCHyMmAVT0" , 
+  authDomain : "videocall-daily.firebaseapp.com" , 
+  projectId : "videocall-daily" , 
+  storageBucket : "videocall-daily.firebasestorage.app" , 
+  messagingSenderId : "547221478499" , 
+  appId : "1:547221478499:web:93e36251ea70786da9ec56" , 
+  measurementId : "G-1EKWJ0NDER" 
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, googleProvider };
